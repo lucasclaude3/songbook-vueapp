e77b02733d5620a936176c9ef3892868ec8c63db
kahlua <lucasclaude3@gmail.com>
2018-10-26 17:43:53 +0200

    Add signin form to the modal

--- src/components/SigninModal.vue
+++ src/components/SigninModal.vue
@@ -1,5 +1,56 @@
 <template>
-    <b-modal id="sign-in" title="Sign in">
+    <b-modal id="sign-in" title="Sign in" @ok="handleOk">
         <p class="my-4">Ola que tal</p>
+            <b-form-group
+                id="fieldset1"
+                description="Let us know your name."
+                label="Enter your name"
+                label-for="input1"
+                :invalid-feedback="invalidFeedback"
+                :valid-feedback="validFeedback"
+                :state="state"
+            >
+
+            <b-form-input id="input1" :state="state" v-model.trim="username"></b-form-input>
+            <b-form-input id="input2" :state="state" v-model.trim="password" type="password"></b-form-input>
+        </b-form-group>
     </b-modal>
 </template>
+
+<script>
+export default {
+  computed: {
+    state () {
+      return ((this.username.length > 0) && (this.password.length > 0)) ? true : false
+    },
+    invalidFeedback () {
+      if (!this.username) {
+          return 'You must specify your username';
+      }
+      if (!this.password) {
+          return 'Please enter your password';
+      }
+      return '';
+    },
+    validFeedback () {
+      return this.state ? 'Thank you' : ''
+    }
+  },
+  data () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    handleOk (evt) {
+        // Prevent modal from closing
+        evt.preventDefault();
+        if (this.invalidFeedback.length > 0) {
+            alert(this.invalidFeedback);
+        }
+        this.handleSubmit();
+    },
+  }
+}
+</script>
