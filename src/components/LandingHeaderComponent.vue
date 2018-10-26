70f2b5d1289876a53a51cecc8571a9fa01f16147
kahlua <lucasclaude3@gmail.com>
2018-10-26 17:02:20 +0200

    Add signin modal

--- src/components/LandingHeaderComponent.vue
+++ src/components/LandingHeaderComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <b-navbar class="ml-auto">
         <b-navbar-brand>
             <img src="./../assets/songbook.png" alt="BV" width="30">
@@ -6,9 +7,15 @@
         </b-navbar-brand>
         <div class="sign-buttons">
             <b-button class="signup">Sign up</b-button>
-            <b-button class="signin">Sign in</b-button>
+            <b-button class="signin" v-b-modal.sign-in>Sign in</b-button>
         </div>
     </b-navbar>
+
+    <b-modal id="sign-in" title="Sign in">
+        <p class="my-4">Ola que tal</p>
+    </b-modal>
+
+</div>
 </template>
 
 <style scoped>
