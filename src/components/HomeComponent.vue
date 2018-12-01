09e88617ac5255f332bedafad3f645bb12bc6e9c
kahlua <lucasclaude3@gmail.com>
2018-12-01 16:45:22 +0100

    Load songs on personal home

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,8 +1,34 @@
 <template>
-    <div>HOME</div>
+    <div>
+      <h1>HOME</h1>
+      <ul id='songs'>
+        <li v-for="song in songs" :key="song.id">{{ song.songName }}</li>
+      </ul>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {
+      songs: [],
+    }
+  },
+  mounted() {
+    axios.get(
+      'http://localhost:3000/songs',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      })
+      .then((response) => { this.songs = response.data; });
+  }
+}
 </script>
 
 <style scoped>
